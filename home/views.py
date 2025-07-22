840d9fa3d8a7981087dc1538412cfe1f1965edba
Gabriel Pimentel <131470415+TheParousia@users.noreply.github.com>
2025-07-22 17:58:30 -0300

    Inserção do teste de recebimento via POST

--- home/views.py
+++ home/views.py
@@ -2,4 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    context = {
+        'result': 'Welcome to the Home Page!',
+        'data': 'Welcome to the Home Page!',
+    }    
+
+    if request.method == 'POST':
+        # Process any POST data if needed
+        context['result'] = 'You submitted a POST request!'
+        context['data'] = request.POST.get('data', 'No data provided')
+        
+    return render(request, 'home.html', context)
